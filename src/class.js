935d1438062b3bf07987fd20b4c5e8b30b8cd485
linjilei <linjilei@qq.com>
2017-08-22 12:09:05 +0800

    调整核心方法. 定义map方法

---
+++ src/class.js
@@ -0,0 +1,15 @@
+/**
+ * 类操作
+ */
+
+export function addClass () {
+  return 1
+}
+
+export function removeClass () {
+
+}
+
+export function hasClass (className) {
+  return this
+}
--- test/collection.test.js
+++ test/collection.test.js
@@ -1,32 +1,30 @@
 import $ from '../src/index'
 import {expect} from 'chai'
 import {beforeEach} from './helper'
-import {
-  length,
-  eq
-} from '../src/collection'
 
 describe('Collection', function () {
   this.beforeEach(beforeEach)
 
-  it('length', function () {
-    expect(length.call($('div'))).to.be.equal(3)
-  })
-
   it('eq', function () {
-    const div = eq.call($('div'), 1)
-    expect(div.length).to.be.equal(1)
-    expect(div[0].className).to.be.equal('rDOM')
+    const $div = $('div').eq(1)
+    expect($div.length).to.be.equal(1)
+    expect($div[0].className).to.be.equal('rDOM')
   })
 
   it('eq minus index', function () {
-    const div = eq.call($('div'), -2)
-    expect(div.length).to.be.equal(1)
-    expect(div[0].className).to.be.equal('rDOM')
-    expect(div[0]).to.be.equal(eq.call($('div'), 1)[0])
+    const $div = $('div').eq(-2)
+    expect($div.length).to.be.equal(1)
+    expect($div[0].className).to.be.equal('rDOM')
+    expect($div[0]).to.be.equal($('div').eq(1)[0])
   })
 
-  it('forEach', function () {
-
+  it('map', function () {
+    const $div = $('div')
+    $div.map((elem, index) => {
+      expect(elem.nodeType).to.be.equal(1)
+      expect(index).to.be.a('number')
+      expect(index).to.most(3)
+      expect(index).to.be.above(-1)
+    })
   })
 })
